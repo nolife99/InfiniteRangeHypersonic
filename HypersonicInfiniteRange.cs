87a287748f6adfe8fa373ce44d46b86da51d4e36
nolife99 <chendylan32@gmail.com>
2023-02-12 16:39:12 -0500

    update

--- HypersonicInfiniteRange.cs
+++ HypersonicInfiniteRange.cs
@@ -1,15 +1,20 @@
-using MelonLoader;
 using BTD_Mod_Helper;
-using System.Linq;
-using HypersonicInfiniteRange;
-using Assets.Scripts.Models;
-using Assets.Scripts.Models.Towers.Behaviors.Attack;
-using HarmonyLib;
 using BTD_Mod_Helper.Api.Data;
 using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Api.ModOptions;
+using Il2CppAssets.Scripts.Models;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
+using Il2Cpp;
 
-[assembly: MelonInfo(typeof(ModMain), ModHelperData.Name, ModHelperData.Version, ModHelperData.Version)]
+using HarmonyLib;
+using MelonLoader;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+using HypersonicInfiniteRange;
+
+[assembly: MelonInfo(typeof(ModMain), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace HypersonicInfiniteRange;
@@ -18,24 +23,29 @@ public sealed class ModMain : BloonsTD6Mod
 {
     public override void OnApplicationStart()
     {
-        LoggerInstance.Msg("Loaded hypersonic!");
-        HarmonyInstance.Patch(AccessTools.Method(typeof(GameModelLoader), nameof(GameModelLoader.Load)), 
-            postfix : new(AccessTools.Method(typeof(ModMain), nameof(ModMain.OnGameLoad))));
+        MelonLogger.Msg("Loaded hypersonic!");
+        var instance = new HarmonyLib.Harmony("69");
+        instance.Patch(AccessTools.Method(typeof(GameModelLoader), nameof(GameModelLoader.Load)), 
+            postfix: new(AccessTools.Method(typeof(ModMain), nameof(ModMain.OnGameLoad))));
     }
     public static void OnGameLoad(ref GameModel __result) => __result.towers.AsParallel().ForAll(t =>
     {
         if (Settings.InfiniteRange) t.range = 999;
         t.behaviors.Do(m =>
         {
-            var potentialAttack = m.TryCast<AttackModel>();
-            if (potentialAttack != null)
+            var attack = m.TryCast<AttackModel>();
+            if (attack != null)
             {
-                if (Settings.InfiniteRange) potentialAttack.range = 999;
-                var wep = potentialAttack.weapons;
+                if (Settings.InfiniteRange)
+                {
+                    attack.range = 999;
+                    attack.attackThroughWalls = true;
+                }
+
+                var wep = attack.weapons;
                 if (Settings.Hypersonic) wep.Do(w => w.rate = 0);
 
-                var length = wep.Length;
-                for (var i = 0; i < length; i++) wep = wep.AddItem(wep[i]).ToArray();
+                Parallel.For(0, wep.Length, i => wep = wep.AddItem(wep[i]).ToArray());
             }
         });
     });
