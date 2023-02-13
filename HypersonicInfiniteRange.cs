af269d4cd2339f701598ac66329d3cd94ac66d97
nolife99 <chendylan32@gmail.com>
2023-02-13 10:15:58 -0500

    help me

--- HypersonicInfiniteRange.cs
+++ HypersonicInfiniteRange.cs
@@ -2,9 +2,9 @@ using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api.Data;
 using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Api.ModOptions;
+
 using Il2CppAssets.Scripts.Models;
 using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
-using Il2Cpp;
 
 using HarmonyLib;
 using MelonLoader;
@@ -13,6 +13,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using HypersonicInfiniteRange;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
 
 [assembly: MelonInfo(typeof(ModMain), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -21,29 +23,22 @@ namespace HypersonicInfiniteRange;
 
 public sealed class ModMain : BloonsTD6Mod
 {
-    public override void OnApplicationStart()
+    public override void OnNewGameModel(GameModel gameModel) => gameModel.towers.AsParallel().ForAll(t =>
     {
-        MelonLogger.Msg("Loaded hypersonic!");
-        var instance = new HarmonyLib.Harmony("69");
-        instance.Patch(AccessTools.Method(typeof(GameModelLoader), nameof(GameModelLoader.Load)), 
-            postfix: new(AccessTools.Method(typeof(ModMain), nameof(ModMain.OnGameLoad))));
-    }
-    public static void OnGameLoad(ref GameModel __result) => __result.towers.AsParallel().ForAll(t =>
-    {
-        if (Configs.InfiniteRange) t.range = 999;
+        if (Configs.InfiniteRange) t.range = 1000;
         t.behaviors.Do(m =>
         {
             var attack = m.TryCast<AttackModel>();
             if (attack != null)
             {
-                if (Settings.InfiniteRange)
+                if (Configs.InfiniteRange)
                 {
-                    attack.range = 999;
+                    attack.range = 1000;
                     attack.attackThroughWalls = true;
                 }
 
                 var wep = attack.weapons;
-                if (Settings.Hypersonic) wep.Do(w => w.rate = 0);
+                if (Configs.Hypersonic) wep.Do(w => w.rate = 0);
 
                 Parallel.For(0, wep.Length, i => wep = wep.AddItem(wep[i]).ToArray());
             }
@@ -53,12 +48,12 @@ public sealed class ModMain : BloonsTD6Mod
     {
         internal static readonly ModSettingBool Hypersonic = new(true)
         {
-            displayName = "Enable hypersonic (requires restart)",
+            displayName = "Enable hypersonic",
             icon = VanillaSprites.FasterBarrelSpinUpgradeIcon
         };
         internal static readonly ModSettingBool InfiniteRange = new(true)
         {
-            displayName = "Enable infinite range (requires restart)",
+            displayName = "Enable infinite range",
             icon = VanillaSprites.EpicRangeUpgradeIcon
         };
     }
